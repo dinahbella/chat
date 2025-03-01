cc17ed29dbf20b5ae248ecb676da22533ecfe089
Dinah Blessing <gocitymobileapp1@gmail.com>
2025-03-01 12:57:00 +0100

    update

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,8 +1,8 @@
 import express from "express";
 import {
   //   getOtherUsers,
-  //   login,
-  //   logout,
+  login,
+  logout,
   register,
 } from "../controllers/userController.js";
 // import isAuthenticated from "../middleware/isAuthenticated.js";
@@ -10,8 +10,8 @@ import {
 const router = express.Router();
 
 router.route("/register").post(register);
-// router.route("/login").post(login);
-// router.route("/logout").get(logout);
+router.route("/login").post(login);
+router.route("/logout").get(logout);
 // router.route("/").get(isAuthenticated, getOtherUsers);
 
 export default router;
