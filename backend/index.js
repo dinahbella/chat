c789b68003195d34f67de048c9990a1fb105197c
Dinah Blessing <gocitymobileapp1@gmail.com>
2025-03-01 13:37:05 +0100

    updates

--- backend/index.js
+++ backend/index.js
@@ -3,12 +3,15 @@ import dotenv from "dotenv";
 import connectDB from "./config/db.js";
 import userRoute from "./routes/userRoute.js";
 import messageRoute from "./routes/messageRoute.js";
+import cookieParser from "cookie-parser";
 
 dotenv.config({});
 const app = express();
 const PORT = process.env.PORT || 4000;
 // middleware
 app.use(express.json());
+app.use(cookieParser());
+
 // routes
 app.use("/api/user", userRoute);
 app.use("/api/message", messageRoute);
